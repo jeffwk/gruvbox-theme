852547b872f8185cd4753489e2a110b4896c4d9a
Jeff Workman <jeff.workman@gmail.com>
2019-11-04 05:40:04 -0500

    Set transparent bg

--- gruvbox-dark-medium-theme.el
+++ gruvbox-dark-medium-theme.el
@@ -57,7 +57,7 @@
  ((((class color) (min-colors #xFFFFFF))        ; col 1 GUI/24bit
    ((class color) (min-colors #xFF)))           ; col 2 Xterm/256
 
-  (gruvbox-dark0           "#282828" "#262626")
+  (gruvbox-dark0           "#282828" "#282828")
   (gruvbox-dark0_soft      "#32302f" "#303030")
   (gruvbox-dark1           "#3c3836" "#3a3a3a")
   (gruvbox-dark2           "#504945" "#4e4e4e")
@@ -105,7 +105,9 @@
   (gruvbox-aquamarine4     "#83A598" "#87af87")
   (gruvbox-turquoise4      "#61ACBB" "#5fafaf")
 
-  (gruvbox-bg gruvbox-dark0))
+  (gruvbox-bg nil)
+  ;; (gruvbox-bg nil)
+  )
 
  (custom-theme-set-variables 'gruvbox-dark-medium
                              `(ansi-color-names-vector
@@ -117,7 +119,7 @@
                                 ,gruvbox-bright_purple
                                 ,gruvbox-bright_aqua
                                 ,gruvbox-light1])
-			     `(pdf-view-midnight-colors '(,gruvbox-light0 . ,gruvbox-bg))))
+                             `(pdf-view-midnight-colors '(,gruvbox-light0 . ,gruvbox-bg))))
 
 ;;;###autoload
 (and load-file-name
